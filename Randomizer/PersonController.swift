40028507435477dbe73f13795c9339af173f734d
srussellh <shanerherbert@gmail.com>
2019-06-14 09:24:20 -0600

    first commit, adds table view, adds model

---
+++ Randomizer/PersonController.swift
@@ -0,0 +1,14 @@
+//
+//  PersonController.swift
+//  Randomizer
+//
+//  Created by Bobba Kadush on 6/14/19.
+//  Copyright © 2019 SRUSSELLH. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class PersonController {
+    
+}
--- Randomizer/RandomizeViewController.swift
+++ Randomizer/RandomizeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RandomizeViewController.swift
 //  Randomizer
 //
 //  Created by Bobba Kadush on 6/14/19.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RandomizeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
